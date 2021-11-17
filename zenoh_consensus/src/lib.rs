eca9b1f3c9e6f603e0d7a42dcfba638170a70065
Yi-Hung Kuo <50904241+eurc17@users.noreply.github.com>
2021-11-17 23:01:15 +0800

    Added documentation for lib.rs

--- zenoh_consensus/src/lib.rs
+++ zenoh_consensus/src/lib.rs
@@ -1,10 +1,20 @@
+/// A bounded counter implementation using stateful CRDT.
 pub mod b_counter;
+/// Common packages used in this crate.
 pub mod common;
+/// A simple implementation of eventual queue.
 pub mod eventual_queue;
+/// A simple implementation of Lamport mutex.
 pub mod lamport_mutex;
+/// A negative/positive counter implementation using stateful CRDT.
 pub mod np_counter;
+/// A negative/positive counter implementation using stateful CRDT, using HashMap as the underlying data structure.
 pub mod np_counter_hashmap;
+/// An implementation of reliable broadcast proposed in [Byzantine Agreement with Unknown Participants and Failures](https://arxiv.org/abs/2102.10442).
 pub mod reliable_broadcast;
+/// An implementation of the Road Side Unit(RSU) that helps monitor the consensus of traffic in an intersection.
 pub mod rsu;
+/// Utilities used in this crate.
 pub mod utils;
+/// Abstraction on sending messages using Zenoh.
 pub mod zenoh_sender;
