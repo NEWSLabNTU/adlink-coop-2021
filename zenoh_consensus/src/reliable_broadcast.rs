e77a3abdccc05e19355ac9cc524cc05aefaf17f8
Yi-Hung Kuo <50904241+eurc17@users.noreply.github.com>
2021-11-22 11:16:44 +0800

    Added documentation for reliable_broadcast.rs

--- zenoh_consensus/src/reliable_broadcast.rs
+++ zenoh_consensus/src/reliable_broadcast.rs
@@ -7,18 +7,26 @@ use tokio_stream::wrappers::ReceiverStream;
 
 use message::*;
 
+/// The `send_timeout` for the repeating echo sender.
 const REPEATING_SEND_PERIOD: Duration = Duration::from_millis(15);
+/// The jitter added for sending repeating echo messages in microseconds.
 const JITTER_MICROS: u64 = 5000;
 
 #[derive(Debug, Clone, PartialEq, Eq, Hash)]
+/// Defines the structure of the config file.
 pub struct Config {
+    /// The maximum number of rounds to run the reliable broadcast.
     pub max_rounds: usize,
+    /// The number of extra rounds to send echo(m,s). It will not exceed the `max_rounds`
     pub extra_rounds: usize,
+    /// The timeout for receiving first 1/3Nv echo messages.
     pub recv_timeout: Duration,
+    /// The timeout for each round. Must be larger than 2 * `recv_timeout`.
     pub round_timeout: Duration,
 }
 
 impl Default for Config {
+    /// Function that returns a default configuration.
     fn default() -> Self {
         Self {
             max_rounds: 3,
@@ -29,6 +37,13 @@ impl Default for Config {
     }
 }
 
+/// Creates a new reliable broadcast instance
+///
+/// Returns a Sender handle and a Receiver handle to send and receive messages.
+/// * `zenoh`: A zenoh instance wrapped in Arc (Atomic Reference Counter).
+/// * `path`: A Borrow typed [zenoh::Path] that indicates the workspace where reliable broadcast happens.
+/// * `id`: A str that identifies the participant of reliable broadcast.
+/// * `config`: The config file for the reliable broadcast. It should be the same for all participants of the same reliable broadcast.
 pub fn new<T>(
     zenoh: Arc<Zenoh>,
     path: impl Borrow<zenoh::Path>,
@@ -108,22 +123,30 @@ where
 
 #[derive(Derivative)]
 #[derivative(Debug)]
+/// A Sender for reliable broadcast.
 pub struct Sender<T>
 where
     T: 'static + Send + Sync + Serialize + DeserializeOwned,
 {
+    /// The id of the sender peer.
     id: String,
+    /// The sequence number of the message.
     seq_counter: AtomicUsize,
     #[derivative(Debug = "ignore")]
+    /// The zenoh instance for sending and receiving messages.
     zenoh: Arc<Zenoh>,
+    /// The [zenoh::Path] to send the message to.
     send_key: zenoh::Path,
     // recv_selector: zenoh::Selector,
+    /// The abstract type of a Zenoh Message sender to help send messages.
     zenoh_tx: ZenohSender<Message<T>>,
-    #[derivative(Debug = "ignore")]
+    // #[derivative(Debug = "ignore")]
     // peers: Arc<DashSet<String>>,
+    /// The timeout for each round of reliable broadcast.
     round_timeout: Duration,
     // contexts: Arc<DashMap<(String, usize), Context<T>>>,
     #[derivative(Debug = "ignore")]
+    /// The [HLC] instance for timestamp creation.
     hlc: Arc<HLC>,
 }
 
@@ -131,6 +154,10 @@ impl<T> Sender<T>
 where
     T: 'static + Send + Sync + Serialize + DeserializeOwned,
 {
+    /// A function that sends the message.
+    ///
+    /// It automatically tag the message with a random sequence number and a timestamp.
+    /// * `data`: The message content to be sent.
     pub async fn send(&self, data: T) -> Result<()> {
         let seq = self.seq_counter.fetch_add(1, Ordering::SeqCst);
         let msg: Message<_> = Broadcast {
@@ -156,11 +183,13 @@ where
 
 #[derive(Derivative)]
 #[derivative(Debug)]
+/// A receiver for reliable broadcast.
 pub struct Receiver<T>
 where
     T: 'static + Send + Sync + Serialize + DeserializeOwned,
 {
     #[derivative(Debug = "ignore")]
+    /// A stream of received messages.
     stream: Pin<Box<dyn Send + Stream<Item = Result<T>>>>,
 }
 
@@ -168,11 +197,30 @@ impl<T> Receiver<T>
 where
     T: 'static + Send + Sync + Serialize + DeserializeOwned,
 {
+    /// A function to receive messages.
+    ///
+    /// Returns a Result of Option of message data. If there is no message, it will be None.
     pub async fn recv(&mut self) -> Result<Option<T>> {
         self.stream.next().await.transpose()
     }
 }
 
+/// A worker that helps to deal with message *(m, s)* and *present* and counts N<sub>v</sub> for the peer.
+///
+/// It creates a [coordinate_worker] to listen for *echo(m, s)* if received a *(m, s)* type message.
+/// * `id`: The ID of the receiver.
+/// * `zenoh`: The zenoh instance for sending and receiving messages.
+/// * `max_rounds`: The maximum number of rounds in a reliable broadcast.
+/// * `extra_rounds`: The number of extra rounds to send echo(m,s). It will not exceed the `max_rounds`.
+/// * `recv_timeout`: The timeout for receiving first 1/3Nv echo messages.
+/// * `round_timeout`: The timeout for each round. Must be larger than 2 * `recv_timeout`.
+/// * `send_key`: The [zenoh::Path] to send the message to.
+/// * `recv_selector`: The [zenoh::Selector] indicating where to receive the messages from.
+///  `job_tx`: The transmitter to send [coordinate_worker] futures to a job polling worker named [await_worker].
+/// * `zenoh_tx`: The zenoh sending handle to send messages.
+/// * `peers`: The set of peers which the current peer has heard at least one message from.
+/// * `contexts`: A set of contexts. Each context deal with each peer.
+/// * `hlc`: A [HLC] instance for timestamp creation.
 async fn recv_worker<T>(
     id: String,
     zenoh: Arc<Zenoh>,
@@ -196,7 +244,7 @@ where
     let mut change_stream = workspace.subscribe(&recv_selector).await?;
 
     debug!("{} is listening", id);
-    
+
     zenoh_tx
         .send(
             Present {
@@ -206,14 +254,14 @@ where
         )
         .await?;
     debug!("{} sends present", id);
-    
+
     // eprintln!("recv_worker = {:?}", Instant::now());
 
     let mut pending_echos = HashMap::new();
 
     'stream_loop: while let Some(change) = change_stream.next().await {
         let peer_name = change.path.last_segment();
-        
+
         match change.kind {
             zenoh::ChangeKind::Put => {
                 peers.insert(peer_name.to_string());
@@ -350,6 +398,22 @@ where
     Ok(())
 }
 
+/// A worker that is spawned to deal with *echo(m, s)* typed messages with a *(m, s)* typed message is received by [recv_worker].
+///
+/// Returns a message if the *(m, s)* message is accepted.
+/// * `id`: The ID of the [coordinate_worker] owner.
+/// * `seq`: The sequence number of the receiving message.
+/// * `sender`: The ID of the sender of the message *(m, s)*.
+/// * `max_rounds`: The maximum number of rounds in a reliable broadcast.
+/// * `extra_rounds`: The number of extra rounds to send echo(m,s). It will not exceed the `max_rounds`.
+/// * `recv_timeout`: The timeout for receiving first 1/3Nv echo messages.
+/// * `round_timeout`: The timeout for each round. Must be larger than 2 * `recv_timeout`.
+/// * `peers`: The set of peers which the current peer has heard at least one message from.
+/// * `hlc`: A [HLC] instance for timestamp creation.
+/// * `zenoh`: The zenoh instance for sending and receiving messages.
+/// * `send_key`: The [zenoh::Path] to send the message to.
+/// * `worker_rx`: The receiver to receive all *echo(m, s)* messages once it is received by [recv_worker].
+/// * `data`: The message data in *(m, s)* received by [recv_worker].
 async fn coordinate_worker<T>(
     id: String,
     seq: usize,
@@ -562,6 +626,9 @@ where
     }
 }
 
+/// A worker that actually spawns the [coordinate_worker] when received the future from [recv_worker] and sends the accepted messages from the [coordinate_worker] to the [Receiver] instance.
+/// * `job_rx`: The receiver to receive `coordinator_worker` futures from [recv_worker].
+/// * `output_tx`: The transmitter to send the accepted messages to the [Receiver].
 async fn await_worker<T>(
     mut job_rx: mpsc::Receiver<Pin<Box<dyn Send + Future<Output = Result<Option<Msg<T>>>>>>>,
     output_tx: mpsc::Sender<Msg<T>>,
@@ -603,6 +670,13 @@ async fn await_worker<T>(
     Ok(())
 }
 
+/// A worker that repeatedly sends *echo(m, s)* messages.
+/// * `zenoh`: The zenoh instance for sending and receiving messages.
+/// * `key`: The [zenoh::Path] to send the message to.
+/// * `send_timeout`: The timeout for sending each *echo(m, s)*.
+/// * `start`: (Optional) A starting time to send the first repeating echo messages. If not specified, it will start as soon as possible.
+/// * `until`: The end time for the worker.
+/// * `data`: The data of the message *(m, s)* that need to be echoed.
 async fn repeating_send<T>(
     zenoh: Arc<Zenoh>,
     key: zenoh::Path,
@@ -657,36 +731,54 @@ where
 
 #[derive(Derivative)]
 #[derivative(Debug)]
+/// The structure to store the context of the dealing progress of each message (m, s) for each peer.
 struct Context<T> {
+    /// The flag indicating if the context has been finished dealing with or not.
     finished: bool,
+    /// The transmitter that sends *echo(m, s)* to [coordinate_worker] if [recv_worker] received one.
     worker_tx: mpsc::Sender<EchoNotify>,
+    /// (Currently not used).
     _phantom: PhantomData<T>,
 }
 
 #[derive(Debug, Clone, Serialize, Deserialize)]
+/// The structure that defines the message *(m, s)*.
 pub struct Msg<T> {
+    /// The ID of the sender.
     pub sender: String,
+    /// The sequence number of the message.
     pub seq: usize,
+    /// The data that is sent in the message.
     pub data: Option<T>,
 }
 
+/// A module that performs operations related to messages.
 pub mod message {
     use super::*;
 
     #[derive(Debug, Clone)]
+    /// A structure that defines the echo notification.
     pub struct EchoNotify {
+        /// The ID of the peer sending the *echo(m, s)* message.
         pub peer: String,
+        /// The timestamp of the message *echo(m, s)*.
         pub timestamp: uhlc::Timestamp,
     }
 
     #[derive(Debug, Clone, Serialize, Deserialize)]
+    /// The enumeration of all types of messages used in reliable broadcast.
     pub enum Message<T> {
+        /// The message type of *(m, s)*.
         Broadcast(Broadcast<T>),
+        /// The message type of *present*.
         Present(Present),
+        /// The message type of *echo(m, s)*
         Echo(Echo),
     }
 
     impl<T> Message<T> {
+        /// A function that obtains the timestamp of the messages.
+        /// Returns a reference to [uhlc::Timestamp].
         pub fn timestamp(&self) -> &uhlc::Timestamp {
             match self {
                 Self::Broadcast(msg) => &msg.timestamp,
@@ -716,29 +808,39 @@ pub mod message {
 
     #[derive(Debug, Clone, PartialEq, Eq, Derivative, Serialize, Deserialize)]
     #[derivative(Hash)]
+    /// The structure for the message type *(m, s)*.
     pub struct Broadcast<T> {
+        /// The sequence number of the message.
         pub seq: usize,
         #[serde(with = "utils::serde_uhlc_timestamp")]
         #[derivative(Hash(hash_with = "utils::hash_uhlc_timestamp"))]
+        /// The timestamp of the message.
         pub timestamp: uhlc::Timestamp,
+        /// The data to send in the message.
         pub data: T,
     }
 
     #[derive(Debug, Clone, PartialEq, Eq, Derivative, Serialize, Deserialize)]
     #[derivative(Hash)]
+    /// The structure for the message type *echo(m, s)*
     pub struct Echo {
+        /// The sequence number of the message.
         pub seq: usize,
+        /// The ID of the message sender of (m, s).
         pub sender: String,
         #[serde(with = "utils::serde_uhlc_timestamp")]
         #[derivative(Hash(hash_with = "utils::hash_uhlc_timestamp"))]
+        /// The timestamp of the message *echo(m, s)*
         pub timestamp: uhlc::Timestamp,
     }
 
     #[derive(Debug, Clone, PartialEq, Eq, Derivative, Serialize, Deserialize)]
     #[derivative(Hash)]
+    /// The structure for the message type *present*.
     pub struct Present {
         #[serde(with = "utils::serde_uhlc_timestamp")]
         #[derivative(Hash(hash_with = "utils::hash_uhlc_timestamp"))]
+        /// The timestamp of the message.
         pub timestamp: uhlc::Timestamp,
     }
 }
@@ -780,11 +882,15 @@ mod tests {
         };
         let zenoh_dir = &zenoh_dir;
         eprintln!("{:?}", Instant::now());
-        let until =
-            Instant::now() + Duration::from_millis((round_timeout_ms * (max_rounds+extra_rounds+10) + 120*num_peers + 100*num_msgs + 800) as u64);
-        
-        let start_until = 
-            Instant::now() + Duration::from_millis((800 + 120*num_peers) as u64); // wait till all peers are ready
+        let until = Instant::now()
+            + Duration::from_millis(
+                (round_timeout_ms * (max_rounds + extra_rounds + 10)
+                    + 120 * num_peers
+                    + 100 * num_msgs
+                    + 800) as u64,
+            );
+
+        let start_until = Instant::now() + Duration::from_millis((800 + 120 * num_peers) as u64); // wait till all peers are ready
 
         let futures = (0..num_peers).map(|peer_index| async move {
             let mut config = zenoh::ConfigProperties::default();
@@ -815,7 +921,8 @@ mod tests {
                     for seq in 0..num_msgs {
                         let mut rand_jitter: u8 = rng.gen();
                         rand_jitter = rand_jitter % 50;
-                        async_std::task::sleep(Duration::from_millis(100+rand_jitter as u64)).await;
+                        async_std::task::sleep(Duration::from_millis(100 + rand_jitter as u64))
+                            .await;
 
                         let data: u8 = rng.gen();
                         eprintln!("{} sends seq={}, data={}", name, seq, data);
