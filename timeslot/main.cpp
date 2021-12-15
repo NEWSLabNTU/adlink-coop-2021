6ae2fe5fa51d67a576afb678ec1ffcb87466fc0a
jeff-901 <b07901052@ntu.edu.tw>
2021-12-15 20:17:10 +0800

    deal with conflict request

--- timeslot/main.cpp
+++ timeslot/main.cpp
@@ -4,6 +4,7 @@
 #include <string>
 #include <vector>
 #define DEBUG 1
+
 struct Route {
     int id;
     int startLaneId;
@@ -12,6 +13,11 @@ struct Route {
 };
 struct Lane {
     int id;
+    int totalCost;
+    int outDegree;
+    int averageCost;
+    bool remove;
+    int unhandledRequest;
     std::vector<Route> routes;
 };
 struct Intersection {
@@ -28,6 +34,7 @@ struct Request {
     int frontCarId;
     int distanceBetweenFrontCar;
     int routeId;
+    int laneId;
     int startTime;
     int endTime;
 };
@@ -51,17 +58,26 @@ bool compareRequest(Request a, Request b) {
     return a.startTime < b.startTime; // 降序排列
 }
 int main(int argc, char *argv[]) {
-    // TODO check argc valid
-    // TODO check file open successfully
     // TODO check roadsite definition constrain
     // TODO check request constrain
+    if (argc != 3) {
+        std::cout
+            << "Usage: [binary file] [lane definition file] [request file]\n";
+        return 1;
+    }
     std::ifstream laneFile;
     laneFile.open(argv[1]);
+    if (!laneFile) {
+        std::cout << "Unable to open the " << argv[1] << "\n";
+        return 1;
+    }
     Intersection intersection;
     laneFile >> intersection.id >> intersection.numberOfLane;
     for (int i = 0; i < intersection.numberOfLane; i++) {
         Lane lane;
         lane.id = i;
+        lane.remove = false;
+        lane.unhandledRequest = 0;
         intersection.lanes.push_back(lane);
     }
     int routeLen;
@@ -100,10 +116,18 @@ int main(int argc, char *argv[]) {
         }
         std::cout << std::endl;
     }
+    for (int i = 0; i < intersection.numberOfLane; i++) {
+        std::cout << "[lane] id: " << intersection.lanes[i].id
+                  << ", remove: " << intersection.lanes[i].remove << std::endl;
+    }
 // ----------------end output--------------
 #endif
     std::ifstream requestFile;
     requestFile.open(argv[2]);
+    if (!requestFile) {
+        std::cout << "Unable to open the " << argv[2] << "\n";
+        return 1;
+    }
     std::vector<Request> requests;
     while (!requestFile.eof()) {
         Request request;
@@ -111,6 +135,8 @@ int main(int argc, char *argv[]) {
             request.carLength >> request.carSpeed >> request.frontCarId >>
             request.distanceBetweenFrontCar >> request.routeId >>
             request.startTime >> request.endTime;
+        request.laneId = intersection.routes[request.routeId].startLaneId;
+        intersection.lanes[request.laneId].unhandledRequest += 1;
         requests.push_back(request);
     }
     std::sort(requests.begin(), requests.end(), compareRequest);
@@ -119,6 +145,107 @@ int main(int argc, char *argv[]) {
         printRequest(requests[i]);
     }
 #endif
+    std::vector<std::vector<int>> conflictEdge(
+        intersection.numberOfLane,
+        std::vector<int>(intersection.numberOfLane, 0));
 
+    // conflictEdge.reserve(intersection.numberOfLane);
+    // for (int i = 0; i < intersection.numberOfLane; i++) {
+    //     conflictEdge[i].reserve(intersection.numberOfLane);
+
+    // }
+    std::vector<Response> responses;
+    int nextIdx = 0;
+    while (nextIdx < requests.size()) {
+        std::cout << "nextId: " << nextIdx << std::endl;
+        int currentIdx = nextIdx + 1;
+        int lastTime = requests[nextIdx].endTime;
+        std::vector<Request> conflicts;
+        conflicts.push_back(requests[nextIdx]);
+        for (int k = 0; k < intersection.numberOfLane; k++) {
+            intersection.lanes[k].averageCost = 0;
+            intersection.lanes[k].outDegree = 0;
+            intersection.lanes[k].totalCost = 0;
+            for (int j = k + 1; j < intersection.numberOfLane; j++) {
+                conflictEdge[k][j] = 0;
+                conflictEdge[j][k] = 0;
+            }
+        }
+        int conflictPossibleIdx = 0;
+        while (currentIdx < requests.size() &&
+               requests[currentIdx].startTime <= lastTime) {
+            std::cout << "currentId: " << currentIdx << std::endl;
+            if (intersection
+                    .lanes[intersection.routes[requests[currentIdx].routeId]
+                               .startLaneId]
+                    .remove) {
+                Response response;
+                response.routeId = requests[currentIdx].routeId;
+                response.carId = requests[currentIdx].carId;
+                response.status = Response::Status::rejected;
+                response.startTime = -1;
+                response.endTime = -1;
+                responses.push_back(response);
+                intersection.lanes[requests[currentIdx].laneId]
+                    .unhandledRequest -= 1;
+            } else {
+                conflicts.push_back(requests[currentIdx]);
+                lastTime = std::max(lastTime, requests[currentIdx].endTime);
+                for (int k = conflictPossibleIdx; k < conflicts.size() - 1;
+                     k++) {
+                    if (conflicts[k].endTime <=
+                        requests[currentIdx].startTime) {
+                        if (priority[conflicts[k].routeId]
+                                    [requests[currentIdx].routeId] != 0) {
+                            int laneId1 =
+                                intersection.routes[conflicts[k].routeId]
+                                    .startLaneId;
+                            int laneId2 =
+                                intersection
+                                    .routes[requests[currentIdx].routeId]
+                                    .startLaneId;
+                            if (conflictEdge[laneId1][laneId2] == 0) {
+                                conflictEdge[laneId1][laneId2] = 1;
+                                conflictEdge[laneId2][laneId1] = 1;
+                                intersection.lanes[laneId1].totalCost +=
+                                    intersection.lanes[laneId2]
+                                        .unhandledRequest;
+                                intersection.lanes[laneId1].outDegree += 1;
+                                intersection.lanes[laneId2].totalCost +=
+                                    intersection.lanes[laneId1]
+                                        .unhandledRequest;
+                                intersection.lanes[laneId2].outDegree += 1;
+                            }
+                        } else if (conflicts[k].routeId ==
+                                       requests[currentIdx].routeId &&
+                                   conflicts[k].startTime >=
+                                       requests[currentIdx].startTime) {
+                            conflicts.pop_back();
+                            Response response;
+                            response.routeId = requests[currentIdx].routeId;
+                            response.carId = requests[currentIdx].carId;
+                            response.status = Response::Status::rejected;
+                            response.startTime = -1;
+                            response.endTime = -1;
+                            responses.push_back(response);
+                            intersection.lanes[requests[currentIdx].laneId]
+                                .unhandledRequest -= 1;
+                        }
+                    } else {
+                        conflictPossibleIdx = k;
+                    }
+                }
+            }
+            currentIdx += 1;
+        }
+// TODO remove node
+#ifdef DEBUG
+        for (std::vector<Request>::iterator it = conflicts.begin();
+             it != conflicts.end(); it++) {
+            printRequest(*it);
+        }
+#endif
+        nextIdx = currentIdx;
+    }
     return 0;
 }
