845e389d583d5863f4ee3785a8d92ab106d4ff61
aeon <jerry73204@gmail>
2021-12-27 16:57:54 +0800

    Tidy benchmark code

--- zenoh-benchmark/src/main.rs
+++ zenoh-benchmark/src/main.rs
@@ -3,87 +3,97 @@ use anyhow::Result;
 use collected::SumVal;
 use futures::future;
 use futures::stream::StreamExt as _;
+use itertools::iproduct;
 use std::sync::Arc;
 use std::time::Duration;
 use std::time::Instant;
 use zenoh as zn;
 
+const KEY: &str = "/key";
+
 #[async_std::main]
 async fn main() {
-    const KEY: &str = "/key";
+    pretty_env_logger::init();
 
-    for n_peers in 2..=10 {
-        for payload_size in (8..=(8 * 256)).step_by(8) {
-            eprintln!("# n={} s={}", n_peers, payload_size);
+    let n_peers_iter = 2usize..=10;
+    let payload_size_iter = (8..=(8 * 256)).step_by(8);
 
-            let workers = (0..n_peers).map(|_| {
-                async_std::task::spawn(async move {
-                    let zenoh = Arc::new(zn::Zenoh::new(zn::net::config::default()).await?);
+    for (n_peers, payload_size) in iproduct!(n_peers_iter, payload_size_iter) {
+        run_test(n_peers, payload_size).await;
+    }
+}
 
-                    let producer_task = {
-                        let zenoh = zenoh.clone();
+async fn run_test(n_peers: usize, payload_size: usize) {
+    eprintln!("# n={} s={}", n_peers, payload_size);
 
-                        async move {
-                            let workspace = zenoh.workspace(None).await?;
-                            workspace
-                                .put(&KEY.try_into()?, vec![0u8; payload_size].into())
-                                .await?;
+    // spawn a worker for each peer
+    let workers = (0..n_peers).map(|_| {
+        async_std::task::spawn(async move {
+            // start zenoh session
+            let zenoh = Arc::new(zn::Zenoh::new(zn::net::config::default()).await?);
 
-                            Result::<_, Error>::Ok(())
-                        }
-                    };
+            // run producer and consumer simultanously
+            // note that producer and consumer share a common Zenoh session
+            let producer_future = producer(zenoh.clone(), payload_size);
+            let consumer_future = consumer(zenoh.clone(), payload_size);
 
-                    let consumer_task = {
-                        let zenoh = zenoh.clone();
+            let instant = Instant::now();
+            let ((), n_received) = futures::try_join!(producer_future, consumer_future)?;
+            let elapsed = instant.elapsed();
 
-                        async move {
-                            let timeout = Duration::from_millis(5000);
+            // close zenoh session
+            let zenoh = Arc::try_unwrap(zenoh).map_err(|_| ()).unwrap();
+            zenoh.close().await?;
 
-                            let workspace = zenoh.workspace(None).await?;
-                            let stream = workspace.subscribe(&KEY.try_into()?).await?;
+            Result::<_, Error>::Ok((n_received, elapsed))
+        })
+    });
 
-                            let n_received = stream
-                                .take(n_peers)
-                                .take_until({
-                                    async move {
-                                        async_std::task::sleep(timeout).await;
-                                    }
-                                })
-                                .filter(|change| future::ready(change.kind == zn::ChangeKind::Put))
-                                .count()
-                                .await;
+    // wait for all peers to complete
+    let result = future::try_join_all(workers).await;
 
-                            Result::<_, Error>::Ok(n_received)
-                        }
-                    };
-
-                    let instant = Instant::now();
-                    let ((), n_received) = futures::try_join!(producer_task, consumer_task)?;
-                    let elapsed = instant.elapsed();
-
-                    let zenoh = Arc::try_unwrap(zenoh).map_err(|_| ()).unwrap();
-                    zenoh.close().await?;
-
-                    Result::<_, Error>::Ok((n_received, elapsed))
-                })
-            });
-
-            let result = future::try_join_all(workers).await;
-
-            match result {
-                Ok(vec) => {
-                    let (total_received, total_elapsed): (SumVal<usize>, SumVal<Duration>) =
-                        vec.into_iter().unzip();
-                    let total_expect = n_peers.pow(2);
-                    let recv_rate = total_received.into_inner() as f64 / total_expect as f64;
-                    let mean_elapsed = total_elapsed.into_inner().div_f64(n_peers as f64);
-                    eprintln!("loss rate {:.2}%", recv_rate * 100.0);
-                    eprintln!("mean elapsed time {:?}", mean_elapsed);
-                }
-                Err(err) => {
-                    eprintln!("error: {:?}", err);
-                }
-            }
+    // print report
+    match result {
+        Ok(vec) => {
+            let (total_received, total_elapsed): (SumVal<usize>, SumVal<Duration>) =
+                vec.into_iter().unzip();
+            let total_expect = n_peers.pow(2);
+            let recv_rate = total_received.into_inner() as f64 / total_expect as f64;
+            let mean_elapsed = total_elapsed.into_inner().div_f64(n_peers as f64);
+            eprintln!("loss rate {:.2}%", recv_rate * 100.0);
+            eprintln!("mean elapsed time {:?}", mean_elapsed);
+        }
+        Err(err) => {
+            eprintln!("error: {:?}", err);
         }
     }
+}
+
+async fn producer(zenoh: Arc<zn::Zenoh>, payload_size: usize) -> Result<()> {
+    let workspace = zenoh.workspace(None).await?;
+    workspace
+        .put(&KEY.try_into()?, vec![0u8; payload_size].into())
+        .await?;
+
+    Ok(())
+}
+
+async fn consumer(zenoh: Arc<zn::Zenoh>, n_peers: usize) -> Result<usize> {
+    let timeout = Duration::from_millis(5000);
+
+    let workspace = zenoh.workspace(None).await?;
+    let stream = workspace.subscribe(&KEY.try_into()?).await?;
+
+    let n_received = stream
+        .take(n_peers)
+        .take_until({
+            async move {
+                async_std::task::sleep(timeout).await;
+            }
+        })
+        .filter(|change| future::ready(change.kind == zn::ChangeKind::Put))
+        .count()
+        .await;
+
+    Ok(n_received)
 }
